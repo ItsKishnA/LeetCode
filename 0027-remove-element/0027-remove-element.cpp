4fed46c798c4f79584d77dd2195c7e2ed54e7537
Avi <avi.knw@gmail.com>
2025-03-26 01:16:45 +0530

    Time: 0 ms (100%), Space: 11.8 MB (4.85%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -3,35 +3,16 @@ public:
     int removeElement(vector<int>& nums, int val) {
         int k=0;
         int lastIter = nums.size()-1;
-        
-
-        // ORIGNAL
-        // for(int j=0; j<nums.size(); j++){
-        // cout<<nums[j]<<", ";
-        // }
-        // cout<<endl;
 
         for(int i=0, temp=0; i<nums.size(); i++){
-            // DEBUGGING
-            // cout<<"from : ";
-            // for(int j=0; j<nums.size(); j++){
-            //     cout<<nums[j]<<", ";
-            // }
-            // cout<<"... to ... : ";
-                
             if(nums[i]==val){
                 // SWIPING VALUES WITH LAST ELEMENT
                 temp = nums[i];
                 nums[i--] = nums[lastIter];
                 nums[lastIter--] = temp;
                 k++;
-
-                // DEBUGGING
-                // for(int j=0; j<nums.size(); j++){
-                //     cout<<nums[j]<<", ";
-                // }
-                // cout<<endl;
             }
+
             if(lastIter == i) break;
         }
         return (nums.size()-k);
