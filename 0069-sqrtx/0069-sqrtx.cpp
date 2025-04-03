7ab6f0170e8a117d7a60a0713aeadb243ebbc186
Avi <avi.knw@gmail.com>
2025-04-04 01:16:25 +0530

    Time: 19 ms (6.98%), Space: 8.7 MB (14.75%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        int sqrt=0;
-
+        // For Zero Case
         if(x==0)return 0;
 
+        // For random int handling
         for(long int i=0; i<=x; i++){
             if((i*i)>x)
-            return i-1;
-            else if((i*i)==x){
+                return i-1;
+            else if((i*i)==x)
                 return i;
-            }
         }
         return 1;
     }
